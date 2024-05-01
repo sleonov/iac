4dfd2fdc96372073f12e94b85875cacbff276569
Sergey Leonovich <sergey.leonovich@transunion.com>
2024-04-30 21:07:42 -0500

    Add static-website

---
+++ terraform/cloud-guru-tf-class-advanced/static-website/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "sleonov-green"
+  region  = "us-east-1"
+  default_tags {
+    tags = {
+      "Environment" = "Test"
+      "Project"     = "Terraform"
+    }
+  }
+}
