941f3e8fd9a8778295972177d582e91aab522913
Sergey Leonovich <sergey.leonovich@transunion.com>
2024-05-06 12:09:49 -0500

    Add param-store to show creation of password for mysql

---
+++ terraform/cloud-guru-tf-class-advanced/param-store/password.tf
@@ -0,0 +1,9 @@
+resource aws_ssm_parameter "admin_password" {
+  name = var.path_to_password
+  type = "SecureString"
+  value = data.aws_secretsmanager_random_password.this.random_password
+}
+
+data "aws_secretsmanager_random_password" "this" {
+  password_length = var.password_length
+}
---
+++ terraform/cloud-guru-tf-class-advanced/param-store/variables.tf
@@ -0,0 +1,9 @@
+variable "path_to_password" {
+  type = string
+  default = "/mysql/admin/password"
+}
+
+variable "password_length" {
+  type = number
+  default = 20
+}
