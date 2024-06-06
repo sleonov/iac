d905a6b8afc85a153ca66115d0d1cc9b2d4574a6
Sergey Leonovich <sergey.leonovich@transunion.com>
2024-06-06 11:28:21 -0500

    Update user data

--- terraform/autoscaling/launch-template-asg-alb/scripts/user_data_web.sh
+++ terraform/autoscaling/launch-template-asg-alb/scripts/user_data_web.sh
@@ -2,6 +2,7 @@
 INDEX="/var/www/html/index.html"
 yum install -y httpd
 hostname >> $INDEX
+echo "<p>This instance started on $(date)</p>" >> $INDEX
 chmod 444
 systemctl enable httpd
 systemctl start httpd
